d1946f2dd7180ddec0dc2a64e28fdab948b4c8c3
Christian Carvalho <christian.coliveira@gmail.com>
2022-03-19 06:28:26 -0300

    Merge branch 'master' of https://github.com/chriscoliveira/python_notas_e_moedas_colecao

--- novoscrap.py
+++ novoscrap.py
@@ -1,3 +1,4 @@
+from base64 import encode
 import os
 import re
 from socket import setdefaulttimeout
@@ -24,13 +25,35 @@ class DBase:
         self.c = self.conn.cursor()
 
     def criartabela(self):
-        sql = 'CREATE TABLE IF NOT EXISTS "moedas"( "id" INTEGER UNIQUE,"PAIS" TEXT,"ANO" TEXT,"KRAUSE" TEXT,"VALOR" TEXT,"PERIODO" TEXT,"CIRCULACAO" TEXT,"ASSUNTO" TEXT,"SERIE" TEXT,"SOBERANO" TEXT,"CUNHAGEM" TEXT,"COMPOSICAO" TEXT,"BORDA" TEXT,"FORMATO" TEXT,"ALINHAMENTO" TEXT,"PESO" TEXT,"CONSERVACAO" TEXT,"DIAMETRO" TEXT,"ESPESSURA" TEXT,"ANVERSO" TEXT,"REVERSO" TEXT,"VENDA" TEXT,"CADASTRO" TEXT,"FOTO1" TEXT,"FOTO2" TEXT,PRIMARY KEY("id" AUTOINCREMENT) )'
+        sql = 'CREATE TABLE IF NOT EXISTS "moedas"( "id" INTEGER UNIQUE, "PAIS" TEXT, "ANO" TEXT, "KRAUSE" TEXT, "VALOR" TEXT, "PERIODO" TEXT, "CIRCULACAO" TEXT, "ASSUNTO" TEXT, "SERIE" TEXT, "SOBERANO" TEXT, "CUNHAGEM" TEXT, "COMPOSICAO" TEXT, "BORDA" TEXT, "FORMATO" TEXT, "ALINHAMENTO" TEXT, "PESO" TEXT, "CONSERVACAO" TEXT, "DIAMETRO" TEXT, "ESPESSURA" TEXT, "ANVERSO" TEXT, "REVERSO" TEXT, "VENDA" TEXT, "CADASTRO" TEXT, "FOTO1" TEXT, "FOTO2" TEXT,PRIMARY KEY("id" AUTOINCREMENT) )'
         self.c.execute(sql)
 
-    def cadastrar(self, venda, cunhagem, foto1, foto2, cadastro, pais, ano, krause, valor, periodo, circulacao, assunto, serie, soberano, composicao, borda, formato, alinhamento, peso, diametro, espessura, anverso, reverso):
-        sql = 'INSERT INTO moedas(venda,cunhagem,foto1,foto2,cadastro,pais,ano,krause,valor,periodo,circulacao,assunto,serie,soberano,composicao,borda,formato,alinhamento,peso,diametro,espessura,anverso,reverso) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) '
-        self.c.execute(sql, (venda, cunhagem, foto1, foto2, cadastro, pais, ano, krause, valor, periodo, circulacao,
-                       assunto, serie, soberano, composicao, borda, formato, alinhamento, peso, diametro, espessura, anverso, reverso))
+    def cadastrar(self, venda, cunhagem, foto1, foto2, cadastro, pais, ano, krause, valor, periodo, circulacao, assunto, serie, soberano, composicao, borda, formato, alinhamento, peso, diametro, espessura, anverso, reverso, conservacao):
+        # print(venda, cunhagem, foto1, foto2, cadastro, pais, ano, krause, valor, periodo, circulacao, assunto, serie,
+        #       soberano, composicao, borda, formato, alinhamento, peso, diametro, espessura, anverso, reverso, sep='\n')
+        # sql = 'INSERT INTO moedas(venda,cunhagem,foto1,foto2,cadastro,pais,ano,krause,valor,periodo,circulacao,assunto,serie,soberano,composicao,borda,formato,alinhamento,peso,diametro,espessura,anverso,reverso) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) '
+        # self.c.execute(sql, (venda, cunhagem, foto1, foto2, cadastro, pais, ano, krause, valor, periodo, circulacao,
+        #                assunto, serie, soberano, composicao, borda, formato, alinhamento, peso, diametro, espessura, anverso, reverso))
+        try:
+            sqliteConnection = sqlite3.connect('db_colecao.db')
+            cursor = sqliteConnection.cursor()
+            print("Successfully Connected to SQLite")
+
+            sqlite_insert_query = "INSERT INTO Moedas (venda, cunhagem, foto1, foto2, cadastro, pais, ano, krause, valor, periodo, circulacao, assunto, serie, soberano, composicao, borda, formato, alinhamento, peso, diametro, espessura, anverso, reverso, conservacao) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+            sqlite_data_tuple = (venda, cunhagem, foto1, foto2, cadastro, pais, ano, krause, valor, periodo, circulacao,
+                                 assunto, serie, soberano, composicao, borda, formato, alinhamento, peso, diametro, espessura, anverso, reverso, conservacao)
+            count = cursor.execute(sqlite_insert_query, sqlite_data_tuple)
+            sqliteConnection.commit()
+            print(
+                "Record inserted successfully into SqliteDb_developers table ",  cursor.rowcount)
+            cursor.close()
+
+        except sqlite3.Error as error:
+            print("Failed to insert data into sqlite table", error)
+        finally:
+            if sqliteConnection:
+                sqliteConnection.close()
+                print("The SQLite connection is closed")
 
 
 Banco = DBase('db_colecao.db')
@@ -58,138 +81,148 @@ def captura_itens_pagina(pagina):
     req = requests.get(pagina, headers={'User-Agent': 'Mozilla/5.0'})
     soup = BeautifulSoup(req.content, 'html.parser')
     itens = soup.find_all('a', class_='blue-15')
-    for item in itens:
-        link = 'https://pt.ucoin.net'+item['href']
+    with open('teste.txt', 'a', encoding='utf-8') as f:
+        for item in itens:
+            link = 'https://pt.ucoin.net'+item['href']
+
+            print(link)
+            f.write(pagina+' '+link+'\n')
+            captura_dados_moeda(link)
 
 
 def captura_dados_moeda(link):
-    VENDA, CUNHAGEM, FOTO1, FOTO2, CADASTRO, PAIS, ANO, KRAUSE, VALOR, PERIODO, CIRCULACAO, ASSUNTO, SERIE, SOBERANO, COMPOSICAO, BORDA, FORMATO, ALINHAMENTO, PESO, DIAMETRO, ESPESSURA, ANVERSO, REVERSO = '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''
+    print(link)
+    VENDA, CUNHAGEM, FOTO1, FOTO2, CADASTRO, PAIS, ANO, KRAUSE, VALOR, PERIODO, CIRCULACAO, ASSUNTO, SERIE, SOBERANO, COMPOSICAO, BORDA, FORMATO, ALINHAMENTO, PESO, DIAMETRO, ESPESSURA, ANVERSO, REVERSO, CONSERVACAO = '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''
 
-    with sync_playwright() as p:
-        # browser = p.chromium.launch(
-        #     channel='chrome', headless=False)
-        browser = p.chromium.launch_persistent_context(
-            user_data_dir=os.path.join(os.path.dirname(__file__), 'user_data'), headless=True)
-        page = browser.new_page()
+    # entra na pagina do anime
+    req = requests.get(link, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(req.content, 'html.parser')
 
-        # entra na pagina do anime
-
-        page.goto(link)
-        page.set_default_timeout(0)
-
-        htm = page.content()
-        soup = BeautifulSoup(htm, 'html.parser')
-
+    try:
         VENDA = soup.find('a', class_='pricewj').text
         VENDA = VENDA.replace('Preço: R$ ', '')
-        print(VENDA)
-        try:
-            CUNHAGEM = soup.find('h4').text
-            print(CUNHAGEM)
-        except:
-            CUNHAGEM = ''
+    except:
+        VENDA = ''
+
+    try:
+        CUNHAGEM = soup.find('h4').text
+
+    except:
+        CUNHAGEM = ''
+    try:
         FOTO = soup.find_all('img', id='coin-img1')
+        FOTOS = []
         for i in FOTO:
-            print(i['src'])
-        infopublicacao = soup.find_all('table', class_='my-func-info')
-        soupinfo = BeautifulSoup(str(infopublicacao), 'html.parser')
-        linhasinfo = soupinfo.find_all('tr')
-        for i in linhasinfo:
-            if 'Publicado' in str(i):
-                CADASTRO = str(i).replace(
-                    '<tr><td class="lgray-12">Publicado</td><td class="gray-12">', '').replace('</td></tr>', '')
-                print(CADASTRO)
+            FOTOS.append(i['src'])
+        FOTO1 = FOTOS[0]
+        FOTO2 = FOTOS[1]
+    except:
+        FOTO1 = ''
+        FOTO2 = ''
+    infopublicacao = soup.find_all('table', class_='my-func-info')
+    soupinfo = BeautifulSoup(str(infopublicacao), 'html.parser')
+    linhasinfo = soupinfo.find_all('tr')
+    for i in linhasinfo:
+        if 'Publicado' in str(i):
+            CADASTRO = str(i).replace(
+                '<tr><td class="lgray-12">Publicado</td><td class="gray-12">', '').replace('</td></tr>', '')
+        if 'Conservação' in str(i):
+            # CONSERVACAO = str(i).replace(
+            #     '<tr><td class="lgray-12">Conservação</td><td class="gray-12">', '').replace('</td></tr>', '')
+            try:
+                CONSERVACAO = soupinfo.find('div', class_='wrap').text
+            except:
+                CONSERVACAO = ''
+            # <div class="wrap" title="VF - Soberba">VF - Soberba</div>
+            # pega todas as informacoes da moeda
+            # pega todas as informacoes da moeda
+    info = soup.find_all('table', class_='coin-info')
+    soupinfo = BeautifulSoup(
+        str(info), 'html.parser', from_encoding='utf-8')
+    linhasinfo = soupinfo.find_all('tr')
 
-        # pega todas as informacoes da moeda
-        info = soup.find_all('table', class_='coin-info')
-        soupinfo = BeautifulSoup(str(info), 'html.parser')
-        linhasinfo = soupinfo.find_all('tr')
-        with open('teste.txt', 'w') as f:
+    for linha in linhasinfo:
 
-            for linha in linhasinfo:
-                f.write(str(linha)+'\n')
-                if 'País' in str(linha):
-                    PAIS = str(linha).replace(
-                        '<tr><th>País</th><td>', '').replace('</td></tr>', '')
-                    print(PAIS)
-                if 'Ano' in str(linha):
-                    ANO = str(linha).replace(
-                        '<tr><th>Ano</th><td>', '').replace('</td></tr>', '')
-                    print(ANO)
-                if 'Krause' in str(linha):
-                    KRAUSE = str(linha).replace(
-                        '<tr><th>Número Krause</th><td>', '').replace('</td></tr>', '')
-                    print(KRAUSE)
+        if 'País' in str(linha):
+            PAIS = str(linha).replace(
+                '<tr><th>País</th><td>', '').replace('</td></tr>', '')
 
-                if 'Denominação' in str(linha):
-                    VALOR = str(linha).replace(
-                        '<tr><th>Denominação</th><td>', '').replace('</td></tr>', '')
-                    print(VALOR)
-                if 'Periodo' in str(linha):
-                    PERIODO = str(linha).replace(
-                        '<tr><th>Periodo</th><td>', '').replace('</td></tr>', '')
-                    print(PERIODO)
-                if 'Tipo de moeda' in str(linha):
-                    CIRCULACAO = str(linha).replace(
-                        '<tr><th>Tipo de moeda</th><td>', '').replace('</td></tr>', '')
-                    print(CIRCULACAO)
-                if 'Assunto' in str(linha):
-                    ASSUNTO = str(linha).replace(
-                        '<tr><th>Assunto</th><td>', '').replace('</td></tr>', '')
-                    print(ASSUNTO)
-                if 'Série' in str(linha):
-                    SERIE = str(linha).replace(
-                        '<tr><th>Série</th><td>', '').replace('</td></tr>', '')
-                    print(SERIE)
-                if 'Soberano' in str(linha):
-                    SOBERANO = str(linha).replace(
-                        '<tr><th>Soberano</th><td>', '').replace('</td></tr>', '')
-                    print(SOBERANO)
+        if 'Ano' in str(linha):
+            ANO = str(linha).replace(
+                '<tr><th>Ano</th><td>', '').replace('</td></tr>', '')
 
-                if 'Composição' in str(linha):
-                    COMPOSICAO = str(linha).replace(
-                        '<tr><th>Composição</th><td>', '').replace('</td></tr>', '')
-                    print(COMPOSICAO)
-                if 'Tipo de bordo' in str(linha):
-                    BORDA = str(linha).replace(
-                        '<tr><th>Tipo de bordo</th><td>', '').replace('</td></tr>', '')
-                    print(BORDA)
-                if 'Formato' in str(linha):
-                    FORMATO = str(linha).replace(
-                        '<tr><th>Formato</th><td>', '').replace('</td></tr>', '')
-                    print(FORMATO)
-                if 'Alinhamento' in str(linha):
-                    ALINHAMENTO = str(linha).replace(
-                        '<tr><th>Alinhamento</th><td>', '').replace('</td></tr>', '')
-                    print(ALINHAMENTO)
-                if 'Peso' in str(linha):
-                    PESO = str(linha).replace(
-                        '<tr><th>Peso (gr)</th><td>', '').replace('</td></tr>', '')
-                    print(PESO)
-                if 'Diametro' in str(linha):
-                    DIAMETRO = str(linha).replace(
-                        '<tr><th>Diametro (mm)</th><td>', '').replace('</td></tr>', '')
-                    print(DIAMETRO)
-                if 'Espessura' in str(linha):
-                    ESPESSURA = str(linha).replace(
-                        '<tr><th>Espessura (mm)</th><td>', '').replace('</td></tr>', '')
-                    print(ESPESSURA)
-                if 'Anverso' in str(linha):
-                    ANVERSO = str(linha).replace(
-                        '<tr><th class="nowrap">Anverso</th><td>', '').replace('<span class="lgray-11"> / </span>', ' / ').replace('</td></tr>', '')
-                    print(ANVERSO)
-                if 'Reverso' in str(linha):
-                    REVERSO = str(linha).replace(
-                        '<tr><th class="nowrap">Reverso</th><td>', '').replace('<span class="lgray-11"> / </span>', ' / ').replace('</td></tr>', '')
-                    print(REVERSO)
-            print(VENDA, CUNHAGEM, FOTO1, FOTO2, CADASTRO, PAIS, ANO, KRAUSE, VALOR, PERIODO, CIRCULACAO, ASSUNTO,
-                  SERIE, SOBERANO, COMPOSICAO, BORDA, FORMATO, ALINHAMENTO, PESO, DIAMETRO, ESPESSURA, ANVERSO, REVERSO)
+        if 'Krause' in str(linha):
+            KRAUSE = str(linha).replace(
+                '<tr><th>Número Krause</th><td>', '').replace('</td></tr>', '')
 
+        if 'Denominação' in str(linha):
+            VALOR = str(linha).replace(
+                '<tr><th>Denominação</th><td>', '').replace('</td></tr>', '')
+
+        if 'Periodo' in str(linha):
+            PERIODO = str(linha).replace(
+                '<tr><th>Periodo</th><td>', '').replace('</td></tr>', '')
+
+        if 'Tipo de moeda' in str(linha):
+            CIRCULACAO = str(linha).replace(
+                '<tr><th>Tipo de moeda</th><td>', '').replace('</td></tr>', '')
+
+        if 'Assunto' in str(linha):
+            ASSUNTO = str(linha).replace(
+                '<tr><th>Assunto</th><td>', '').replace('</td></tr>', '')
+
+        if 'Série' in str(linha):
+            SERIE = str(linha).replace(
+                '<tr><th>Série</th><td>', '').replace('</td></tr>', '')
+
+        if 'Soberano' in str(linha):
+            SOBERANO = str(linha).replace(
+                '<tr><th>Soberano</th><td>', '').replace('</td></tr>', '')
+
+        if 'Composição' in str(linha):
+            COMPOSICAO = str(linha).replace(
+                '<tr><th>Composição</th><td>', '').replace('</td></tr>', '')
+
+        if 'Tipo de bordo' in str(linha):
+            BORDA = str(linha).replace(
+                '<tr><th>Tipo de bordo</th><td>', '').replace('</td></tr>', '')
+
+        if 'Formato' in str(linha):
+            FORMATO = str(linha).replace(
+                '<tr><th>Formato</th><td>', '').replace('</td></tr>', '')
+
+        if 'Alinhamento' in str(linha):
+            ALINHAMENTO = str(linha).replace(
+                '<tr><th>Alinhamento</th><td>', '').replace('</td></tr>', '')
+
+        if 'Peso' in str(linha):
+            PESO = str(linha).replace(
+                '<tr><th>Peso (gr)</th><td>', '').replace('</td></tr>', '')
+
+        if 'Diametro' in str(linha):
+            DIAMETRO = str(linha).replace(
+                '<tr><th>Diametro (mm)</th><td>', '').replace('</td></tr>', '')
+
+        if 'Espessura' in str(linha):
+            ESPESSURA = str(linha).replace(
+                '<tr><th>Espessura (mm)</th><td>', '').replace('</td></tr>', '')
+
+        if 'Anverso' in str(linha):
+            ANVERSO = str(linha).replace(
+                '<tr><th class="nowrap">Anverso</th><td>', '').replace('<span class="lgray-11"> / </span>', ' / ').replace('</td></tr>', '')
+
+        if 'Reverso' in str(linha):
+            REVERSO = str(linha).replace(
+                '<tr><th class="nowrap">Reverso</th><td>', '').replace('<span class="lgray-11"> / </span>', ' / ').replace('</td></tr>', '')
+
+    Banco.cadastrar(VENDA, CUNHAGEM, FOTO1, FOTO2, CADASTRO, PAIS, ANO, KRAUSE, VALOR, PERIODO, CIRCULACAO, ASSUNTO,
+                    SERIE, SOBERANO, COMPOSICAO, BORDA, FORMATO, ALINHAMENTO, PESO, DIAMETRO, ESPESSURA, ANVERSO, REVERSO, CONSERVACAO)
+
+
+for i in range(26, 111):
+    captura_itens_pagina(link_colecao+str(i))
 
 # captura_itens_pagina(link_colecao+'1')
-captura_dados_moeda(
-    'https://pt.ucoin.net/coin/usa-1-cent-2017/?ucid=44236499')
-# 'https://pt.ucoin.net/coin/united_kingdom-1-penny-1988/?ucid=47106083')
-
-
+# captura_dados_moeda(
+    # 'https://pt.ucoin.net/coin/spain-10-centimos-1941/?ucid=50926539')
 # login_site()
